b5855ae3d9e07a8feb6fac7a9bc4fed22d540bbc
Kalpesh <kalpesh@connou.app>
2024-09-22 21:21:07 +0530

    done till anonymous user (not solved yet).

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -1,8 +1,14 @@
+from django.contrib import admin
 from django.urls import path, include
 from homeapp import views
 
+# To change admin portal display text
+# admin.site.site_header = "CWH Icecream Admin"
+# admin.site.site_title = "CWH Icecream Admin Portal"
+# admin.site.index_title = "Welcome to CWH Icecream"
+
 urlpatterns = [
     path('', views.index, name='home'),
-    path('/login', views.login, name='login'),
-    path('/logout', views.logout, name='logout'),
+    path('login', views.login, name='login'),
+    path('logout', views.logoutUser, name='logout'),
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate
 # Create your views here.
+
 def index(request):
+    if request.user.is_authenticated:
+        return redirect('/login')
     return render(request, 'index.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-def logout(request):
-    return render(request, 'logout.html')
+        # Check if user has entered correct credentials
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            return redirect('/')
+        
+        else:
+            print("login here error")
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/login')
